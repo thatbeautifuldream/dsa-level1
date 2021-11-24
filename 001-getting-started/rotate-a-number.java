9bc63214c8bfdf7f527592538a63813d09be394f
Milind Mishra <milind.mishra4@gmail.com>
2021-11-24 18:47:08 +0530

    rotate a number

---
+++ 001-getting-started/rotate-a-number.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int ans = rotateNumber(n, k);
+        System.out.println(ans);
+    }
+
+    public static int countDigits(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n / 10;
+            count++;
+        }
+        return count;
+    }
+
+    public static int rotateNumber(int n, int k) {
+        int len = countDigits(n);
+        k = ((k % len) + len) % len;
+        int div = 1, mul = 1;
+        for (int i = 1; i <= k; i++) {
+            div *= 10;
+        }
+        for (int i = 1; i <= len - k; i++) {
+            mul *= 10;
+        }
+        int a = n / div;
+        int b = n % div;
+        return (b * mul + a);
+    }
+}
