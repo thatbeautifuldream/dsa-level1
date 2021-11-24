3491f455c5a63931e88283bc452212619c04dc0a
Milind Mishra <milind.mishra4@gmail.com>
2021-11-24 18:57:46 +0530

    inverse

---
+++ 001-getting-started/inverse-of-a-number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = printInverse(n);
+        System.out.println(ans);
+        sc.close();
+    }
+
+    public static int printInverse(int num) {
+        int lastIndex = 1, res = 0;
+        while (num != 0) {
+            int lastDigit = num % 10;
+            num /= 10;
+            res += lastIndex * (int) Math.pow(10, lastDigit - 1);
+            lastIndex++;
+        }
+        return res;
+    }
+}
